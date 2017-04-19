b5df69b64b85fdee1b3e929b35881dfe68d4b946
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-04-18 18:22:40 -0600

    Add gradle wrapper plus two empty subprojects

---
+++ storage/src/main/groovy/ncei/catalog/Application.groovy
@@ -0,0 +1,11 @@
+package ncei.catalog
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+  static void main(String[] args) {
+    SpringApplication.run(Application.class, args)
+  }
+}
