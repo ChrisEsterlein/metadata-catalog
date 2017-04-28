bcc8a83d2c40475e8bed06c1af8ae9788a79f5ad
Erin <erin.rowland@colorado.edu>
2017-04-28 14:55:08 -0600

    Changed rabbit test to actually really verify the rabbit message was saved in the repository.

--- index/src/integrationTest/groovy/ncei/catalog/controller/IndexRabbitApiSpec.groovy
+++ index/src/integrationTest/groovy/ncei/catalog/controller/IndexRabbitApiSpec.groovy
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 import spock.lang.Unroll
+import spock.util.concurrent.PollingConditions
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 
@@ -34,13 +35,15 @@ class IndexRabbitApiSpec extends Specification {
     message.setTask('save')
     message.setMetadata(metadata)
 
+    def conditions = new PollingConditions(timeout: 10, initialDelay: 1.5, factor: 1.25)
+
     when:
     rabbitTemplate.convertAndSend(ConsumerConfig.queueName, message)
 
     then:
-    repository.save(message.metadata) == metadata
+    conditions.eventually {
+      assert repository.findAll().size() == 1
+      assert repository.findAll()[0] == metadata
+    }
   }
 }
-
-
-
