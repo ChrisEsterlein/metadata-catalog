a91686fa09fcf5e9da252915ba9d42d4ed82bfef
Zeb <elizabeth.delk@colorado.edu>
2017-05-15 14:49:41 -0600

    Got the unit tests working with mocks.

--- storage/src/test/groovy/ncei/catalog/service/RepoServiceSpec.groovy
+++ storage/src/test/groovy/ncei/catalog/service/RepoServiceSpec.groovy
@@ -1,108 +1,90 @@
 package ncei.catalog.service
 
-import ncei.catalog.domain.CollectionMetadata
-import ncei.catalog.domain.CollectionMetadataRepository
 import ncei.catalog.domain.GranuleMetadata
-import ncei.catalog.domain.GranuleMetadataRepository
-import ncei.catalog.domain.MetadataRecord
-import ncei.catalog.domain.MetadataSchema
-import ncei.catalog.domain.MetadataSchemaRepository
-import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.cassandra.repository.CassandraRepository
-import spock.lang.Ignore
 import spock.lang.Specification
 import spock.lang.Unroll
 
 import javax.servlet.http.HttpServletResponse
 
-@Ignore
+//@CompileStatic
 @Unroll
 class RepoServiceSpec extends Specification {
 
 
-  @Autowired
-  RabbitTemplate rabbitTemplate
-
   RepoService repoService
-  MessageService messageService
-  CassandraRepository granuleMetadataRepository
-  CassandraRepository collectionMetadataRepository
-  CassandraRepository schemaMetadataRepository
-
-  HttpServletResponse response
+  MessageService messageService = Mock()
 
 
+  final def granuleMetadataMap = [
+      "id": UUID.fromString("10686c20-27cc-11e7-9fdf-ef7bfecc6188"),
+      "tracking_id":"test-id-1",
+      "filename" : "test.txt",
+      "dataset": "test-dataset-1",
+      "type":"file",
+      "granule_size":1024,
+      "granule_metadata": "{blah: blah}",
+      "granule_schema":"schema",
+      "geometry" : "point(1.1, 1.1)",
+      "collections":["FOS"]
+  ]
 
   def setup(){
-    repoService = new RepoService()
-    messageService = new MessageService()
-    response = Mock(HttpServletResponse)
-    response = Mock(HttpServletResponse)
-
-//    mockRabbitTemplate = Mock(RabbitTemplate)
-    messageService.rabbitTemplate = rabbitTemplate
-    repoService.messageService = messageService
-    granuleMetadataRepository = Mock(GranuleMetadataRepository)
-    collectionMetadataRepository = Mock(CollectionMetadataRepository)
-    schemaMetadataRepository = Mock(MetadataSchemaRepository)
-
-  }
-
-  Map createDataItem(MetadataRecord metadataRecord){
-    [id: metadataRecord.id, type: getTableFromClass(metadataRecord), attributes: metadataRecord.asMap()]
-  }
-  def getTableFromClass(MetadataRecord metadataRecord) {
-    switch (metadataRecord.class) {
-      case CollectionMetadata:
-        return 'collection'
-        break
-      case GranuleMetadata:
-        return 'granule'
-        break
-      case MetadataSchema:
-        return 'schema'
-        break
-    }
+    repoService = new RepoService(messageService: messageService)
   }
 
   def 'test repoService save'(){
-    setup: 'instantiate a new granuleMetadata pogo'
-    def granuleMetadataMap = [
-            "id": UUID.fromString("10686c20-27cc-11e7-9fdf-ef7bfecc6188"),
-            "tracking_id":"test-id-1",
-            "filename" : "test.txt",
-            "dataset": "test-dataset-1",
-            "type":"file",
-            "granule_size":1024,
-            "granule_metadata": "{blah: blah}",
-            "granule_schema":"schema",
-            "geometry" : "point(1.1, 1.1)",
-            "collections":["FOS"]
-    ]
+    setup: 'findByMetadataId returns something false'
+
+    CassandraRepository granuleMetadataRepository = Mock()
+    granuleMetadataRepository.metaClass.findByMetadataId = {UUID id ->
+      return null
+    }
+    HttpServletResponse response = Mock()
 
     GranuleMetadata granuleMetadata = new GranuleMetadata(granuleMetadataMap)
 
-    Map serviceResponse =  [data: createDataItem(granuleMetadata), meta: [action:'insert']]
-
-    when:
+    when: 'calling service save'
     Map result = repoService.save(response, granuleMetadataRepository, granuleMetadata)
 
-    then:
-    //the service will try to find the record, which does not exist
-    1 * granuleMetadataRepository.findByMetadataId(granuleMetadata.id) >> null
-
-    //so it will create a new one
+    then: 'the metadata is saved to the repository'
     1 * granuleMetadataRepository.save(granuleMetadata) >> granuleMetadata
 
-    1 * messageService.notifyIndex(serviceResponse)
+    and: 'an insert notification is sent'
+    1 * messageService.notifyIndex({
+      it.meta.action == 'insert'
+    })
 
-//    1 * messageService.rabbitTemplate.convertAndSend('index-consumer', serviceResponse)
-
-//    1 * mockRabbitTemplate.convertAndSend(_)
-    //and build the appropriate response
-    result == serviceResponse
-
+    and: 'the status is good'
+    1 * response.setStatus(HttpServletResponse.SC_CREATED)
+    result.meta.action == 'insert'
+    result.errors == null
   }
 
+  def 'test repoService save conflict'(){
+    setup: 'findByMetadataId returns an object'
+    CassandraRepository granuleMetadataRepository = Mock()
+    HttpServletResponse response = Mock()
+
+
+    GranuleMetadata granuleMetadata = new GranuleMetadata(granuleMetadataMap)
+
+    granuleMetadataRepository.metaClass.findByMetadataId = {UUID id ->
+      [granuleMetadata]
+    }
+
+    when: 'calling service save'
+    Map result = repoService.save(response, granuleMetadataRepository, granuleMetadata)
+
+    then: 'nothing is saved'
+    0 * granuleMetadataRepository.save(granuleMetadata) >> granuleMetadata
+
+    and: 'there is no notification'
+    0 * messageService.notifyIndex(_)
+
+    and: 'the conflict is returned'
+    1 * response.setStatus(HttpServletResponse.SC_CONFLICT)
+    result.meta.action == 'insert'
+    !result.errors.isEmpty()
+  }
 }
