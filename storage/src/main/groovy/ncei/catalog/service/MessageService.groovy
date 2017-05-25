6c89dbb335d3f017f32e6d8dbc0f72cde813aa00
Erin <erin.rowland@colorado.edu>
2017-05-25 10:48:34 -0600

    Changed hardcoded rabbit queue name to use the routingKey configuration value.

--- storage/src/main/groovy/ncei/catalog/service/MessageService.groovy
+++ storage/src/main/groovy/ncei/catalog/service/MessageService.groovy
@@ -3,6 +3,7 @@ package ncei.catalog.service
 import groovy.util.logging.Slf4j
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Slf4j
@@ -12,10 +13,13 @@ class MessageService {
   @Autowired
   RabbitTemplate rabbitTemplate
 
+  @Value ('${rabbitmq.routingKey}')
+  String routingKey
+
   void notifyIndex(Map details) {
     try{
       log.info "Notifying index of action"
-      rabbitTemplate.convertAndSend('index-consumer', details)
+      rabbitTemplate.convertAndSend(routingKey, details)
     }catch(e){
       log.error('Failed to notify index with exception: ', e)
     }
