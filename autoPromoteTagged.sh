7cca0cf9c883f4d741c4511a43f59172260d6be3
Zeb <elizabeth.delk@colorado.edu>
2017-06-09 09:42:03 -0600

    Use bash -c to run short inline bash script instead of introducing another script file.

--- autoPromoteTagged.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This checks if the tag name starts with 'v', allowing other tags to be used without accidentally triggering an artifact promotion.
-if [[ $(git tag -l --points-at HEAD) == v* ]]; then
-  ./gradelw promote
-fi
